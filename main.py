6d84779c87ea3b42bfb1a9acfa2fa84713690107
Mickilenzy <ilerioluwamichael297@gmail.com>
2025-02-06 11:59:54 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+number = int(input("input a number"))
+if (number%2==0):
+    print("even number")
+else: 
+    print("odd number")
+
+    height =float(input("enter your height"))
+    weight =float(input("enter your height"))
+
+    BMI - weight/ (height/100)**2
+
+    print("Your BMI is: " , BMI)
+
+    if BMI <=18.4:
+        print("You are underweight")
+    elif BMI <= 24.9:
+        print("overweight")
+    elif BMI <=45.7:
+        print("obese")
+
+              
